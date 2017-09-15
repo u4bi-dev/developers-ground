31e86f8577fcb76d3a92d25971a80717eab28b95
u4bi <u4bi@u4bi.com>
2017-09-16 02:27:32 +0900

    toolbar component prototype render and main component prototype render
    - toolbar.component.ts
    - main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -8,16 +7,9 @@ import { Router } from '@angular/router';
 })
 export class MainComponent implements OnInit {
 
-  constructor(
-    private _router : Router
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onMain(){
-    this._router.navigate(['/main']);
-    console.log('on main');
   }
 
 }
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor(private _router : Router) { }
+
+  ngOnInit() {
+  }
+
+  onMain(){
+    this._router.navigate(['/main']);
+    console.log('on main');
+  }
+
+}
