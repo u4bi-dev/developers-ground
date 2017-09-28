8bdd0435d8fa4bd26e8ca0cb598fb6746dcca44c
u4bi <u4bi@u4bi.com>
2017-09-28 09:48:11 +0900

    toolbar component event scroll background color render

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit,Inject } from '@angular/core';
 import { CoreService } from '../../providers/core.service';
+import { DOCUMENT } from '@angular/platform-browser';
+import { Router, NavigationStart } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,10 +10,27 @@ import { CoreService } from '../../providers/core.service';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+  
+  scroll : Object = {};
+  routerUrl : string; 
 
-  constructor(public core : CoreService) { }
+  constructor(public core : CoreService, @Inject(DOCUMENT) private document: Document, private router: Router) {
+    this.router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe((event:NavigationStart) => {
+        this.routerUrl = event.url;
+        this.scroll = this.routerUrl === '/main' ? { 'background' : 'transparent', 'box-shadow' : 'none' } : { 'background' : '#4E2A84', 'box-shadow' : '0 4px 4px rgba(0, 0, 0, 0.24), 0 0 4px rgba(0, 0, 0, 0.12)' }
+      });
 
-  ngOnInit() {
   }
   
+  ngOnInit() {
+    
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.scroll = (document.body.scrollTop < 270 && this.routerUrl === '/main') ? { 'background' : 'transparent', 'box-shadow' : 'none' } : { 'background' : '#4E2A84', 'box-shadow' : '0 4px 4px rgba(0, 0, 0, 0.24), 0 0 4px rgba(0, 0, 0, 0.12)' };
+  }
+
 }
