013eec62ea21bc9a761ca65d15198f8bae4a0f59
u4bi <u4bi@u4bi.com>
2017-09-30 16:57:16 +0900

    main meetup list component render

--- src/app/pages/main/main-meetup-list/main-meetup-list.component.ts
+++ src/app/pages/main/main-meetup-list/main-meetup-list.component.ts
@@ -7,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMeetupListComponent implements OnInit {
 
-  constructor() { }
+  public meetupList : Object;
+
+  constructor() {
+    
+    this.meetupList = [
+        { 
+          image : './assets/icon_favicon.png',
+          title : '목동 커피빈 모각코',
+          address : '서울특별시 양천구 목동',
+          join : 8
+        },
+        { 
+          image : './assets/icon_favicon.png',
+          title : '목동 커피빈 모각코',
+          address : '서울특별시 양천구 목동',
+          join : 8
+        },
+        { 
+          image : './assets/icon_favicon.png',
+          title : '목동 커피빈 모각코',
+          address : '서울특별시 양천구 목동',
+          join : 8
+        },
+        { 
+          image : './assets/icon_favicon.png',
+          title : '목동 커피빈 모각코',
+          address : '서울특별시 양천구 목동',
+          join : 8
+        }
+    ];
+    
+  }
 
   ngOnInit() {
   }
