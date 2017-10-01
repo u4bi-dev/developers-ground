ec501821a603b757832333d2d10f741b3e5dd7c5
u4bi <u4bi@u4bi.com>
2017-10-01 11:28:49 +0900

    fix main header componenet height size

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -30,7 +30,7 @@ export class ToolbarComponent implements OnInit {
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
-    this.scroll = document.body.scrollTop < 270 && ( this.routerUrl === '/main' || this.routerUrl === '/' ) ? { 'background' : 'transparent', 'box-shadow' : 'none' } : { 'background' : '#4E2A84', 'box-shadow' : '0 4px 4px rgba(0, 0, 0, 0.24), 0 0 4px rgba(0, 0, 0, 0.12)' };
+    this.scroll = document.body.scrollTop < 370 && ( this.routerUrl === '/main' || this.routerUrl === '/' ) ? { 'background' : 'transparent', 'box-shadow' : 'none' } : { 'background' : '#4E2A84', 'box-shadow' : '0 4px 4px rgba(0, 0, 0, 0.24), 0 0 4px rgba(0, 0, 0, 0.12)' };
   }
 
 }
